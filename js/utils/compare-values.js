4e0317415de251f337a005d27bd477394ffda3e0
kada <adakennedy6@gmail.com>
2024-09-10 13:34:30 +0300

    update: updating documentation to reflect datatypes passed to x and y

--- js/utils/compare-values.js
+++ js/utils/compare-values.js
@@ -2,17 +2,17 @@
 
 /**
  * Compares two values based on a specified order.
- * @param {number|null} x - The first value to compare.
- * @param {number|null} y - The second value to compare.
+ * @param {number|null|string} x - The first value to compare.
+ * @param {number|null|string} y - The second value to compare.
  * @param {string} order - The order to use for comparison ("asc" for ascending, "desc" for descending).
  * @returns {boolean} - Returns true if `x` should come before `y` based on the order, otherwise false.
  */
 export function compareValues(x, y, order) {
     // If x is null and y is not, x should come after y
-    if (x === null && y !== null) return false;
+    if (x === null && y !== null) return true;
     
     // If y is null and x is not, x should come before y
-    if (x !== null && y === null) return true;
+    if (x !== null && y === null) return false;
 
     // If both are null, they are considered equal, so return false
     if (x === null && y === null) return false;
