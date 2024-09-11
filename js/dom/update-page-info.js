650d7858c75d1e65cffc2ba38aec6f064540dc59
kada <adakennedy6@gmail.com>
2024-09-11 09:52:06 +0300

    fix: fixed bug where if display all data is selected, total pages was displaying as NaN

--- js/dom/update-page-info.js
+++ js/dom/update-page-info.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { getState, setState } from "../state.js";
+import { getState } from "../state.js";
 
 /**
  * Updates the page information display to show the current page and total number of pages.
@@ -11,7 +11,10 @@ export const updatePageInfo = () => {
     const { currentPage, rowsPerPage, filteredData } = getState();
     
     // Calculate the total number of pages
-    const totalPages = Math.ceil(filteredData.length / rowsPerPage);
+    let totalPages = Math.ceil(filteredData.length / rowsPerPage);
+    if ( isNaN(totalPages) ) {
+        totalPages = 1;
+    }
     
     // Set the text content to display current page and total pages
     pageInfo.innerText = `Page ${currentPage} of ${totalPages}`;
